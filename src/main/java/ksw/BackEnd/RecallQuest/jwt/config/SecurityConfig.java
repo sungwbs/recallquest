906d632f9e5dbe10be63aee42a930f08c11a7bb6
최성준 <choi000803@naver.com>
2024-11-25 19:14:57 +0900

    Update SecurityConfig.java

--- src/main/java/ksw/BackEnd/RecallQuest/jwt/config/SecurityConfig.java
+++ src/main/java/ksw/BackEnd/RecallQuest/jwt/config/SecurityConfig.java
@@ -60,6 +60,8 @@ public class SecurityConfig {
                         CorsConfiguration configuration = new CorsConfiguration();
 
                         configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8088"));
+                        configuration.setAllowedOrigins(Collections.singletonList("https://port-0-recallquest-m3vw3lvu42683571.sel4.cloudtype.app"));
+
 //                        configuration.setAllowedOrigins(Collections.singletonList("http://192.210.55.10:8088"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
